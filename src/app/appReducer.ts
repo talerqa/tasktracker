9a4889f7d4ef654c1051cf982e6769313eadd96f
talerqa <hitri3003@gmail.com>
2023-07-06 19:02:17 +0300

    add: 14 todolist

---
+++ src/app/appReducer.ts
@@ -0,0 +1,23 @@
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+  status: 'loading' as RequestStatusType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return {...state, status: action.status}
+    default:
+      return state
+  }
+}
+
+export const setStatus = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+
+
+export type SetStatusType = ReturnType<typeof setStatus>
+type ActionsType = SetStatusType
