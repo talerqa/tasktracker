5ae293923849225605642f67bae0f75264f19148
talerqa <hitri3003@gmail.com>
2023-07-15 19:40:15 +0300

    add: hash router

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,18 @@ import './index.css';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // // If you want to start measuring performance in your app, pass a function
