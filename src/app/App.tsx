b854948a50f25a638380c667824163dba67adcb9
talerqa <hitri3003@gmail.com>
2023-09-10 21:09:52 +0300

    ref: add component Rounting from App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { CircularProgress, Container } from "@mui/material";
-import { Login } from "features/auth/ui/login/login";
-import { TodolistsList } from "features/TodolistsList/ui/TodolistsList";
+import { HashRouter } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 import { ErrorSnackbar } from "common/components";
 import { useActions } from "common/hooks";
 import { selectIsInitialized } from "app/app.selectors";
 import { authThunks } from "features/auth/model/auth.slice";
 import { Header } from "app/Header/Header";
+import { Routing } from "app/Routing/Routing";
 
 function App() {
   const isInitialized = useSelector(selectIsInitialized);
 
-  const { initializeApp, logout } = useActions(authThunks);
+  const { initializeApp } = useActions(authThunks);
 
   useEffect(() => {
     initializeApp();
@@ -32,12 +31,7 @@ function App() {
       <div>
         <ErrorSnackbar />
         <Header />
-        <Container fixed>
-          <Routes>
-            <Route path={"/"} element={<TodolistsList />} />
-            <Route path={"/login"} element={<Login />} />
-          </Routes>
-        </Container>
+        <Routing />
       </div>
     </HashRouter>
   );
---
+++ src/app/Routing/Routing.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { TodolistsList } from "features/TodolistsList/ui/TodolistsList";
+import { Login } from "features/auth/ui/login/login";
+import { Container } from "@mui/material";
+
+export const Routing = () => {
+  return (
+    <Container fixed>
+      <Routes>
+        <Route path={"/"} element={<TodolistsList />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </Container>
+  );
+};
