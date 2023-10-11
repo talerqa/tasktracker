418315a0641bac28dfbf6b3b894942ecac0ec022
talerqa <hitri3003@gmail.com>
2023-10-11 22:51:56 +0300

    fix: bug when add new tasks

--- src/features/TodolistsList/ui/TodolistsList.tsx
+++ src/features/TodolistsList/ui/TodolistsList.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { todolistsThunks } from "features/TodolistsList/model/todolists/todolists.slice";
-import { Grid, Paper } from "@mui/material";
+import { Grid } from "@mui/material";
 import { AddItemForm } from "common/components";
 import { Navigate } from "react-router-dom";
 import { useActions } from "common/hooks";
@@ -13,10 +13,9 @@ import { tasksThunks } from "features/TodolistsList/model/tasks/tasks.slice";
 import s from "./TodolistList.module.css";
 
 export const TodolistsList = () => {
-  const todolists = useSelector(selectTodolists);
   const tasks = useSelector(selectTasks);
   const isLoggedIn = useSelector(selectIsLoggedIn);
-
+  const todolists = useSelector(selectTodolists);
   const { addTodolist: addTodolistThunk, fetchTodolists } = useActions(todolistsThunks);
   const { fetchTasks } = useActions(tasksThunks);
 
@@ -24,12 +23,6 @@ export const TodolistsList = () => {
     if (!isLoggedIn) return;
     fetchTodolists();
   }, [isLoggedIn]);
-
-  useEffect(() => {
-    todolists.forEach((todolist) => {
-      fetchTasks(todolist.id);
-    });
-  }, []);
 
   const addTodolist = useCallback((title: string) => {
     return addTodolistThunk(title).unwrap();
@@ -48,12 +41,11 @@ export const TodolistsList = () => {
       </Grid>
       <Grid container spacing={3} className={s.todolists}>
         {sortedTodoLists.map((tl) => {
-          let allTodolistTasks = tasks[tl.id];
           return (
             <Grid item key={tl.id}>
-              <Paper style={{ padding: "10px" }}>
-                <Todolist todolist={tl} tasks={allTodolistTasks} />
-              </Paper>
+              <div className={s.todolist}>
+                <Todolist todolist={tl} />
+              </div>
             </Grid>
           );
         })}
